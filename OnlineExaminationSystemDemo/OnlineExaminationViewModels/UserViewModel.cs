2dd9a9ea0bb1a5134c4dae099bb9fe3eceabdad3
Faris k.p <fariskp46820@gmail.com>
2024-04-17 11:20:44 +0530

    Basic of  DataAccess layer

---
+++ OnlineExaminationSystemDemo/OnlineExaminationViewModels/UserViewModel.cs
@@ -0,0 +1,47 @@
+﻿using OnlineExamination.DataAccess;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OnlineExaminationViewModels
+{
+    public class UserViewModel
+    {
+        public UserViewModel(Users model)
+        {
+            Id = model.Id;
+            Name = model.Name ?? "";
+            UserName = model.UserName;
+            Password = model.Password;
+            Role = model.Role;
+
+        }
+
+        public Users ConvertViewModel(UserViewModel vm)
+        {
+            return new Users
+            {
+                Id = vm.Id,
+                Name = vm.Name,
+                UserName = vm.UserName,
+                Password = vm.Password,
+                Role = vm.Role
+            };
+        }
+        public int Id { get; set; }
+
+        [Required]
+        [Display(Name = "Name")]
+        public string Name { get; set; }
+        [Required]
+        [Display(Name = "User Name")]
+        public string UserName { get; set; }
+        [Required]
+        public string Password { get; set; }
+
+        public int Role { get; set; }
+    }
+}
